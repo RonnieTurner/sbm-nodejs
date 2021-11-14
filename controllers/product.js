bfea0a06e10518715512054fa28661636656506e
Ronnie Turner <ronnie.turner.303@gmail.com>
2021-11-13 17:39:43 -0700

    Update product.js
    added sequelize.create()

--- controllers/product.js
+++ controllers/product.js
@@ -1,13 +1,26 @@
+const Product = require("../models/product");
+
 exports.postProducts = (req, res, next) => {
   const productName = req.body.productName;
   const productType = req.body.productType;
   const cost = req.body.cost;
+  Product.create({
+    productName: productName,
+    productType: productType,
+    cost: cost,
+  })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   res.status(201).json({
     message: "product added successfully",
     post: {
       productName: productName,
       productType: productType,
       cost: cost,
-    }
-  })
+    },
+  });
 };
